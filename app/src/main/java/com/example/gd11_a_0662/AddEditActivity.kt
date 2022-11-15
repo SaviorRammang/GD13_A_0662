d82d145dddb0219a72e877986bb967858748ba17
SaviorRammang <almindosaviorrammang@gmail.com>
2022-11-15 12:32:30 +0700

    GD11_A_0662

---
+++ app/src/main/java/com/example/gd11_a_0662/AddEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gd11_a_0662
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
